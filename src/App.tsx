834b6d3dd525d33f8a6b116ed0d4bacb5044a8aa
Ilya <gashimovilya2018@gmail.com>
2024-07-18 01:25:49 +0300

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import dataProvider, {
+  GraphQLClient,
+  liveProvider,
+} from "@refinedev/nestjs-query";
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { createClient } from "graphql-ws";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { authProvider } from "./authProvider";
+
+const API_URL = "https://api.nestjs-query.refine.dev/graphql";
+const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
+
+const gqlClient = new GraphQLClient(API_URL);
+const wsClient = createClient({ url: WS_URL });
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <DevtoolsProvider>
+          <Refine
+            dataProvider={dataProvider(gqlClient)}
+            liveProvider={liveProvider(wsClient)}
+            routerProvider={routerBindings}
+            authProvider={authProvider}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              useNewQueryKeys: true,
+              projectId: "GvDCtV-McWGLt-xcw0gP",
+              liveMode: "auto",
+            }}
+          >
+            <Routes>
+              <Route index element={<WelcomePage />} />
+            </Routes>
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+          <DevtoolsPanel />
+        </DevtoolsProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
